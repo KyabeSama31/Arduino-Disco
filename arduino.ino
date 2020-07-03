caf4598fd4cd455f126a7da18ec5285a426076fb
GR <guillaume.ros@epsi.fr>
2020-07-03 17:50:54 +0200

    blink led with button

--- arduino.ino
+++ arduino.ino
@@ -1,9 +1,24 @@
 #include <Arduino.h>
 
+int pinLed = 5;
+int pinButton = 2;
+int buttonState = 0;
 void setup() {
+    Serial.begin(9600);
+    pinMode(pinLed, OUTPUT);
+    pinMode(pinButton, INPUT);
 
 }
 
 void loop() {
+    buttonState = digitalRead(pinButton);
+    Serial.println(buttonState);
+    if (buttonState == HIGH) {
+        digitalWrite(pinLed, HIGH);
+    } else {
+        digitalWrite(pinLed, LOW);
+    }
+    delay(10);
+
 
 }
